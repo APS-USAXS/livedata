2d544a27942b10c4ab3b91548a6398bd5acb2595
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-03-24 21:59:48 +0000

    write report to XML file, include units if available, report periodically, respond faster

--- USAXS/livedata/pvwatch.py
+++ USAXS/livedata/pvwatch.py
@@ -7,7 +7,13 @@
 # $Id$
 ########### SVN repository information ###################
 
-'''watch the USAXS EPICS process variables and write them to a file periodically'''
+'''
+   watch the USAXS EPICS process variables and 
+   write them to a file periodically
+   
+   Start this with the shell command
+   /APSshare/bin/python ./pvwatch.py >>& log.txt
+'''
 
 import time		# provides sleep()
 import datetime		# date/time stamps
@@ -17,7 +23,8 @@ from pvlist import *	# the list of PVs to be watched
 
 REPORT_INTERVAL_S = 30
 SLEEP_INTERVAL_S = 0.2
-REPORT_FILE = "./report.xml"
+REPORT_FILE = "./www/report.xml"
+SVN_ID = "$Id$"
 
 
 pvdb = {}   # EPICS data will go here
@@ -30,7 +37,13 @@ def monitor_receiver(epics_args, user_args):
     pv = ch.GetPv()
     value = ch.GetValue()
     pvdb[pv]['timestamp'] = getTime()
-    #print 'monitor_receiver: ', pv, ' = ', value
+    try:
+        # update the units, if possible
+	if pvdb[pv]['units'] != epics_args['pv_units']:
+            pvdb[pv]['units'] = epics_args['pv_units']
+    except:
+        pass
+    #print 'monitor_receiver: ', pv, ' = ', value, epics_args
 
 
 def add_pv(item):
@@ -44,7 +57,7 @@ def add_pv(item):
     ch.SetUserCallback(monitor_receiver)
     ch.monitor()
     entry = {}
-    entry['pv'] = pv
+    entry['name'] = pv
     entry['id'] = id
     entry['description'] = desc
     entry['timestamp'] = None
@@ -53,18 +66,27 @@ def add_pv(item):
     pvdb[pv] = entry
 
 
+def makeSimpleTag(tag, value):
+    '''create a simple XML tag/value string'''
+    s = "<%s>%s</%s>" % (tag, value, tag)
+    return s
+
 def report():
     '''write the values out in XML'''
     f = open(REPORT_FILE, 'w')
-    f.write('<?xml version="1.0"?>\n')
+    f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    f.write('<?xml-stylesheet type="text/xsl" href="raw-table.xsl" ?>\n')
     f.write('<usaxs_pvs version="1">\n')
-    for pv in pvdb:
+    f.write("  " + makeSimpleTag('writer', SVN_ID) + "\n")
+    f.write("  " + makeSimpleTag('datetime', getTime()) + "\n")
+    sorted_pv_list = sorted(pvdb)
+    for pv in sorted_pv_list:
 	entry = pvdb[pv]
 	ch = entry['ch']
-	f.write('  <pv name="%s">\n' % pv)
-	for item in ("id", "description", "timestamp", "units"):
-	    f.write('    <%s>%s</%s>\n' % (item, entry[item], item))
-	f.write('    <%s>%s</%s>\n' % ("value", ch.GetValue(), "value"))
+	f.write('  <pv id="%s" name="%s">\n' % (entry['id'], pv))
+	for item in ("name", "id", "description", "timestamp", "units"):
+	    f.write("    " + makeSimpleTag(item, entry[item]) + "\n")
+	f.write("    " + makeSimpleTag("value", ch.GetValue()) + "\n")
 	f.write('  </pv>\n')
     f.write('</usaxs_pvs>\n')
     f.close()
@@ -81,6 +103,8 @@ if __name__ == '__main__':
     if pvConnect.IMPORTED_CACHANNEL:
         test_pv = 'S:SRcurrentAI'
         if pvConnect.testConnect(test_pv):
+	    print getTime(), ": starting pvwatch.py"
+
 	    ch = pvConnect.EpicsPv(test_pv)
 	    ch.connectw()
 	    ch.monitor()
