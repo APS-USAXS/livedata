7ee73188f19eb96ae9adc028b2ca62e2b92cd303
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-03-27 21:54:36 +0000

    make this callable from other python

--- USAXS/livedata/lots.py
+++ USAXS/livedata/lots.py
@@ -14,13 +14,13 @@
 import specplot
 PLOT_DIR = "./www/scanplots"
 HTML_FILE = PLOT_DIR + "/index.html"
+SPEC_FILE = '/share1/USAXS_data/2010-03/03_27.dat'
+HREF_FORMAT = "<a href=\"%s\"><img src=\"%s\" width=\"%s\" height=\"%s\" alt=\"%s\"/></a>"
 
-HREF_FORMAT = "<a href=\"%s\"><img src=\"%s\" width=\"%s\" height=\"%s\"/></a>"
 
-if __name__ == '__main__':
-    specFile = '/share1/USAXS_data/2010-03/03_27.dat'
+def plotAllScans(specFile, plotDir="", htmlFile="index.html"):
+    '''plot all the scans found'''
     sd = specplot.openSpecFile(specFile)
-    print len(sd.scans)
     html = []
     html.append('<html>')
     html.append('<head>')
@@ -30,13 +30,20 @@ if __name__ == '__main__':
     html.append("<h1>SPEC scans from: %s</h1>" % specFile)
     for scan in sd.scans:
 	shortName = "s%d.png" % scan.scanNum
-	plotFile = "%s/%s" % (PLOT_DIR, shortName)
+	plotFile = "%s/%s" % (plotDir, shortName)
         print scan.scanNum, scan.scanCmd, plotFile
+	altText = "#%d: %s" % (scan.scanNum, scan.scanCmd)
+	#----- make the plot here
         specplot.makePloticusPlot(specFile, scan, plotFile)
-	html.append(HREF_FORMAT % (shortName, shortName, "150", "75"))
+	#----- and build the index.html page
+	html.append(HREF_FORMAT % (shortName, shortName, "150", "75", altText))
     html.append('</body>')
     html.append('</html>')
     #------------------
-    f = open(HTML_FILE, "w")
+    f = open(htmlFile, "w")
     f.write("\n".join(html))
     f.close()
+
+
+if __name__ == '__main__':
+    plotAllScans(SPEC_FILE, PLOT_DIR, HTML_FILE)
