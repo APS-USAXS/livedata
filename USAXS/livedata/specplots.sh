2a0916a26e9b7759d1be62dcec2b2ba6a5e47109
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-04-21 18:11:03 +0000

    logging

--- USAXS/livedata/specplots.sh
+++ USAXS/livedata/specplots.sh
@@ -15,8 +15,9 @@ SPEC_DATA_PATTERN=/share1/USAXS_data/2010-04/*.dat
 export PLOTICUS_BASE=/home/joule/USAXS/code/pl233src
 export PLOTICUS_PREFABS=/home/joule/USAXS/code/pl233src/prefabs
 
-/bin/date >> $LOGFILE 2>&1;
 cd $WORKING_DIR
+echo "#!-------------" >> $LOGFILE 2>&1
+/bin/date >> $LOGFILE 2>&1
 
 filelist=`/bin/ls -1 $SPEC_DATA_PATTERN`
 for item in $filelist; 
