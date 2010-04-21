90b63aea21bc1b6a85aa1a1af0382995c0c8f773
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-04-21 17:17:12 +0000

    prepare to automate specplots to search a directory tree for revised SPEC data files

---
+++ USAXS/livedata/dirWatch.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+########### SVN repository information ###################
+# $Date: 2010-04-20 17:34:31 -0500 (Tue, 20 Apr 2010) $
+# $Author: jemian $
+# $Revision: 272 $
+# $URL: https://subversion.xor.aps.anl.gov/small_angle/USAXS/livedata/plot.py $
+# $Id: plot.py 272 2010-04-20 22:34:31Z jemian $
+########### SVN repository information ###################
+
+'''
+   watch a directory (and all directories beneath) for new versions of SPEC data files
+'''
+
+import os
+import string
+#import sys
+import time
+
+
+BASE_DIR = "/share1/USAXS_data/"
+SKIP_DIRS = ['.AppleFileInfo']
+KEEP_EXTS = ['.dat']
+TIME_WINDOWS_SECS = 60*60*24*180
+TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class dirWatch:
+    '''get the mtime from SPEC data files under a directory'''
+
+    def __init__(self, dir):
+        '''define the interface components'''
+        self.baseDir = dir
+        self.db = {}
+        self.mtime = -1
+        self.file = ''
+        self.discover()
+        
+    def discover(self):
+        '''walk the directory tree looking for treasures'''
+        if not os.path.exists(self.baseDir):
+            return      # cannot find this item
+        now = time.time()
+        time_window = now - TIME_WINDOWS_SECS
+        for (dirpath, dirnames, filenames) in os.walk(self.baseDir):
+            if os.path.split(os.path.abspath(dirpath))[-1] in SKIP_DIRS:
+                continue    # weed out unwanted directories
+            if len(filenames) == 0:
+                continue    # no files in this directory
+            absdir = os.path.abspath(dirpath)
+            for item in filenames:
+                ext = string.lower(os.path.splitext(item)[-1])
+                if not ext in KEEP_EXTS:
+                    continue    # only look at certain files
+                full = os.path.join(dirpath, item)
+                mtime = os.path.getmtime(os.path.join(absdir, item))
+                if mtime < time_window:
+                    continue    # only examine recent files
+                self.db[full] = mtime
+                if mtime > self.mtime:
+                    self.mtime = mtime
+                    self.file = full
+
+
+def ts2text(ts):
+    '''convert a floating-point timestamp into text'''
+    return time.strftime(TIMESTAMP_FORMAT, time.localtime(ts))
+
+
+if __name__ == '__main__':
+    first = dirWatch(BASE_DIR)
+    ts = ts2text(first.mtime)
+    #print len(first.db), ts, first.file
+    for item in first.db:
+        print ts2text(first.db[item]), item
