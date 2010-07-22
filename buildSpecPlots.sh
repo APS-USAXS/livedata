3c5118c9ad1cb763cf833cff0afcf0784012bdbb
Pete R Jemian <prjemian@users.noreply.github.com>
2010-07-22 04:22:36 +0000

    prepare to regenerate plots for all available USAXS scans

--- buildSpecPlots.sh
+++ buildSpecPlots.sh
@@ -9,8 +9,8 @@
 
 WORKING_DIR=/home/beams/S15USAXS/Documents/eclipse/USAXS/livedata
 LOGFILE=$WORKING_DIR/www/specplots.log
-#SPEC_DATA_PATTERN=/data/USAXS_data/20*-*/*.dat
-SPEC_DATA_PATTERN=/data/USAXS_data/2010-04/*.dat
+SPEC_DATA_PATTERN=/data/USAXS_data/20*-*/*.dat
+#SPEC_DATA_PATTERN=/data/USAXS_data/2010-04/*.dat
 
 export PLOTICUS_BASE=/home/beams/S15USAXS/Documents/ploticus/pl241src
 export PLOTICUS_PREFABS=$PLOTICUS_BASE/prefabs
