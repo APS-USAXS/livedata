194596cdd5d19f6de6ea41329bca7ff46886209c
Pete R Jemian <prjemian@users.noreply.github.com>
2010-07-22 03:44:28 +0000

    seems ready for 15ID - will run next and see...

--- specplots.sh
+++ specplots.sh
@@ -7,20 +7,16 @@
 # $Id$
 ########### SVN repository information ###################
 
-#@TODO: needs to be converted to 15ID-D
+WORKING_DIR=/home/beams/S15USAXS/Documents/eclipse/USAXS/livedata
+LOGFILE=$WORKING_DIR/www/specplots.log
+#SPEC_DATA_PATTERN=/data/USAXS_data/20*-*/*.dat
+SPEC_DATA_PATTERN=/data/USAXS_data/2010-04/*.dat
 
-WORKING_DIR=/home/joule/USAXS/code/livedata
-LOGFILE=$WORKING_DIR/specplots.log
-#SPEC_DATA_PATTERN=/share1/USAXS_data/20*-*/*.dat
-SPEC_DATA_PATTERN=/share1/USAXS_data/2010-04/*.dat
-
-#@TODO: needs ploticus
-
-export PLOTICUS_BASE=/home/joule/USAXS/code/pl233src
-export PLOTICUS_PREFABS=/home/joule/USAXS/code/pl233src/prefabs
+export PLOTICUS_BASE=/home/beams/S15USAXS/Documents/ploticus/pl241src
+export PLOTICUS_PREFABS=$PLOTICUS_BASE/prefabs
 
 cd $WORKING_DIR
-echo "#!-------------" >> $LOGFILE 2>&1
+echo "#=-------------" >> $LOGFILE 2>&1
 /bin/date >> $LOGFILE 2>&1
 
 filelist=`/bin/ls -1 $SPEC_DATA_PATTERN`
