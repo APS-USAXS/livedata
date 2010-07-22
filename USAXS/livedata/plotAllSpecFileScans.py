7cb91036f9142908d126649adeb0dbcb736eab9b
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2010-07-22 03:17:44 +0000

    indentation and break long lines

--- USAXS/livedata/plotAllSpecFileScans.py
+++ USAXS/livedata/plotAllSpecFileScans.py
@@ -23,7 +23,8 @@ PLOT_DIR = "./www/scanplots"
 SPEC_FILE = "/data/USAXS_data/2010-03/03_27.dat"
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 SVN_ID = "$Id$"
-HREF_FORMAT = "<a href=\"%s\"><img src=\"%s\" width=\"%s\" height=\"%s\" alt=\"%s\"/></a>"
+HREF_FORMAT = "<a href=\"%s\"><img src=\"%s\" width=\"%s\" "
+HREF_FORMAT += " height=\"%s\" alt=\"%s\"/></a>"
 HTML_FORMAT = """<html>
   <head>
     <title>SPEC scans from %s</title>
@@ -54,8 +55,6 @@ def plotAllSpecFileScans(specFile):
     yyyy = "%04d" % date[0]
     mm = "%02d" % date[1]
     dd = "%02d" % date[2]
-    #basedir = os.path.join(PLOT_DIR, yyyy, mm, dd, basename)
-    # shorter path if we cut out the dd part
     basedir = os.path.join(PLOT_DIR, yyyy, mm, basename)
     if not os.path.exists(basedir):
         os.makedirs(basedir)
@@ -64,26 +63,31 @@ def plotAllSpecFileScans(specFile):
         basePlotFile = "s%05d.png" % scan.scanNum
         fullPlotFile = os.path.join(basedir, basePlotFile)
         altText = "#%d: %s" % (scan.scanNum, scan.scanCmd)
-        plotList.append(HREF_FORMAT % (basePlotFile, basePlotFile, "150", "75", altText))
+        plotList.append(HREF_FORMAT % (basePlotFile, basePlotFile, 
+                "150", "75", altText))
         #print "specplot.py %s %d %s" % (specFile, scan.scanNum, fullPlotFile)
         remake_plot = True
         if os.path.exists(fullPlotFile):
             # TODO: check this logic
             plotFile_mtime = os.path.getmtime(fullPlotFile)
             if plotFile_mtime > specFile_mtime:
-                # plot was made after the data file was updated, don't remake the plot
+                # plot was made after the data file was updated
+                # don't remake the plot
                 remake_plot = False
         #remake_plot = True  # force new plots for development
         if remake_plot:
             try:
                 specplot.makePloticusPlot(scan, fullPlotFile)
             except:
-                msg = "ERROR: '%s' %s #%d" % (sys.exc_value, specFile, scan.scanNum)
+                msg = "ERROR: '%s' %s #%d" % (sys.exc_value, 
+                        specFile, scan.scanNum)
                 print msg
                 plotList.pop()     # replace the default link
                 plotList.append("<!-- " + msg + " -->")
-                altText = "%s: #%d %s" % (sys.exc_value, scan.scanNum, scan.scanCmd)
-                plotList.append(HREF_FORMAT % (basePlotFile, basePlotFile, "150", "75", altText))
+                altText = "%s: #%d %s" % (sys.exc_value, 
+                        scan.scanNum, scan.scanCmd)
+                plotList.append(HREF_FORMAT % (basePlotFile, 
+                        basePlotFile, "150", "75", altText))
     baseSpecFile = os.path.basename(specFile)
     datestamp = time.strftime(TIMESTAMP_FORMAT, time.localtime(time.time()))
     commentFormat = """
@@ -92,18 +96,19 @@ def plotAllSpecFileScans(specFile):
        date: %s
     """
     comment = commentFormat % (sys.argv[0], SVN_ID, datestamp)
-    html = HTML_FORMAT % (specFile, comment, specFile, baseSpecFile, specFile, '\n'.join(plotList))
+    html = HTML_FORMAT % (specFile, comment, specFile, 
+                baseSpecFile, specFile, '\n'.join(plotList))
     #------------------
     # copy the SPEC data file to the WWW site, only if file has newer mtime
     wwwSpecFile = os.path.join(basedir, baseSpecFile)
     if os.path.exists(wwwSpecFile):
         wwwSpecFile_mtime = os.path.getmtime(wwwSpecFile)
-	if specFile_mtime > wwwSpecFile_mtime:
-	    # specFile is newer, copy to WWW site
-	    shutil.copy2(specFile,wwwSpecFile)
+        if specFile_mtime > wwwSpecFile_mtime:
+            # specFile is newer, copy to WWW site
+            shutil.copy2(specFile,wwwSpecFile)
     else:
         # copy specFile to WWW site
-	shutil.copy2(specFile,wwwSpecFile)
+        shutil.copy2(specFile,wwwSpecFile)
     #------------------
     htmlFile = os.path.join(basedir, "index.html")
     f = open(htmlFile, "w")
