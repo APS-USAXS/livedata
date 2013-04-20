c1ace0f2a285683b317502c74cbbfd40f4f22faa
Pete R Jemian <prjemian@users.noreply.github.com>
2013-04-20 16:01:29 +0000

    use lxml in new code (much easier and richer), read PVs from XML file, show how to do XSLT transformation using lxml

--- v2/pvconnect.py
+++ v2/pvconnect.py
@@ -2,6 +2,53 @@
 
 '''
 connect with and watch EPICS PVs
+
+Usage:
+
+    ::
+    
+        from pvconnect import WatchedList
+        # ...
+        w = WatchedList()
+        w.add('iso8601', 'morel:iso8601', 'timestamp',   '%s')
+        w.add('tod',     'morel:TOD',     'time of day', '%s')
+
+        # then store the current list values in an XML document in `pv` elements
+        # this is an example:
+        from lxml import etree
+        root = etree.Element("usaxs_pvs")
+        root.set("version", "1")
+        root.set("monitored_events", str(w.getTotalCallbacks()))
+        w.write_xml(root, 'pv')
+
+Resulting XML content::
+
+    <usaxs_pvs version="1" monitored_events="22">
+      <pv id="tod" name="morel:TOD">
+        <name>morel:TOD</name>
+        <id>morel:TOD</id>
+        <description>time of day</description>
+        <timestamp>2013-04-20 10:36:29.092000</timestamp>
+        <counter>11</counter>
+        <units></units>
+        <value>04/20/2013 10:37:04</value>
+        <raw_value>04/20/2013 10:37:04</raw_value>
+        <format>%s</format>
+      </pv>
+      <pv id="iso8601" name="morel:iso8601">
+        <name>morel:iso8601</name>
+        <id>morel:iso8601</id>
+        <description>timestamp</description>
+        <timestamp>2013-04-20 10:36:29.373000</timestamp>
+        <counter>11</counter>
+        <units></units>
+        <value>2013-04-20T10:37:05</value>
+        <raw_value>2013-04-20T10:37:05</raw_value>
+        <format>%s</format>
+      </pv>
+    </usaxs_pvs>
+
+        
 '''
 
 ########### SVN repository information ###################
@@ -13,8 +60,7 @@ connect with and watch EPICS PVs
 ########### SVN repository information ###################
 
 
-from xml.dom import minidom
-from xml.etree import ElementTree
+from lxml import etree
 import datetime
 import epics
 import time
@@ -26,7 +72,7 @@ TOTAL_CALLBACKS = 0
 class WatchedPV(object):
     '''connect to and watch an EPICS PV for updates'''
     
-    def __init__(self, mne, name, desc, fmt):
+    def __init__(self, mne, name, desc, fmt='%s'):
         self.name = name            # EPICS PV name
         self.mne =  mne             # symbolic name used in the python code
         self.description = desc     # text description for humans
@@ -60,11 +106,11 @@ class WatchedPV(object):
     
     def write_xml(self, parent, tag = 'pv'):
         '''creates, fills, and returns an XML node named `tag` below `parent`'''
-        node = ElementTree.SubElement(parent, tag)
+        node = etree.SubElement(parent, tag)
         node.set("id", self.mne)
         node.set("name", self.name)
         
-        create = ElementTree.SubElement
+        create = etree.SubElement
         create(node, 'name').text        = str(self.name)
         create(node, 'id').text          = str(self.name)
         create(node, 'description').text = str(self.description)
@@ -85,15 +131,37 @@ class WatchedList(object):
         self.db = {}
         self.xref = {}
     
-    def add(self, mne, name, desc, fmt):
-        self.db[mne] = WatchedPV(mne, name, desc, fmt)
-        self.xref[name] = mne
+    def add(self, mne, pvname, desc, fmt='%s'):
+        '''
+        add a PV to the watch list
+        
+        :param str mne: mnemonic reference (short name), **must be unique**
+        :param str pvname: EPICS process variable name
+        :param str desc: description, readable by humans
+        :param str fmt: display format string (default ``%s``)
+        '''
+        self.db[mne] = WatchedPV(mne, pvname, desc, fmt)
+        self.xref[pvname] = mne
     
-    def getList(self):
-        return [self.db[mne] for mne in sorted(self.db.keys())]
+    def getList(self, sorted=True):
+        '''
+        return a list of all PyEpics objects
+        
+        :param bool sorted: if True (default), sort by `mne`
+        '''
+        if sorted:
+            #keys = sorted(self.db)        # TODO: Why is this failing?
+            return [self.db[mne] for mne in self.db]
+        else:
+            return self.db.values()
     
     def get(self, mne):
+        '''return the PyEpics PV obj for `mne`'''
         return self.db.get(mne, None)
+    
+    def getTotalCallbacks(self):
+        '''return the total number of EPICS callback events received'''
+        return TOTAL_CALLBACKS
     
     def write_xml(self, parent, tag = 'pv'):
         '''adds XML `tag` nodes below `parent` for each PV in the list'''
@@ -102,15 +170,15 @@ class WatchedList(object):
 
 
 if __name__ == '__main__':
+    observation_time = 10
     w = WatchedList()
     w.add('iso8601', 'morel:iso8601', 'timestamp',   '%s')
     w.add('tod',     'morel:TOD',     'time of day', '%s')
-    for _ in range(10):
-        time.sleep(1)
-    print TOTAL_CALLBACKS
+    time.sleep(observation_time)
+    print w.getTotalCallbacks()
     
-    root = ElementTree.Element("usaxs_pvs")
+    root = etree.Element("usaxs_pvs")
     root.set("version", "1")
-    root.set("monitored_events", str(TOTAL_CALLBACKS))
+    root.set("monitored_events", str(w.getTotalCallbacks()))
     w.write_xml(root, 'pv')
-    print minidom.parseString(ElementTree.tostring(root)).toprettyxml(indent = "  ")
+    print etree.tostring(root, pretty_print=True)
