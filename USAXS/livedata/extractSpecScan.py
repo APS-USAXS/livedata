7cbfee6d8bd1cdcf178da74753a52e10777707e1
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2013-11-21 18:55:07 +0000

    refactor, remove duplicate requests, report all mismatched column labels

--- USAXS/livedata/extractSpecScan.py
+++ USAXS/livedata/extractSpecScan.py
@@ -23,9 +23,11 @@ labels are character strings (not valid integers), separated by spaces.
 Compatible with Python 2.6+
 """
 
+
 import prjPySpec
 import os
 import sys
+
 
 #-------------------------------------------------------------------------------------------
 
@@ -54,13 +56,11 @@ def makeOutputFileName(specFile, scanNum):
     return outFile
 
 
-def do_the_work(specFile, scanList, column_labels):
+def extractScans(cmdArgs):
     '''
     read the data file, find each scan, find the columns, save the data
     
-    :param str specFile: name of existing SPEC data file to be read
-    :param [int] scanList: list of chosen SPEC scan numbers
-    :param [str] column_labels: list of column labels
+    :param [str] cmdArgs: command line arguments split into a list, as in sys.argv
     
     .. note:: Each column label must match *exactly* the name of a label
        in each chosen SPEC scan number or the program will skip that particular scan
@@ -75,6 +75,8 @@ def do_the_work(specFile, scanList, column_labels):
         1.9975    65449    478
     
     '''
+    specFile, scanList, column_labels = parseCmdLine(cmdArgs)
+
     print "program: " + sys.argv[0]
     # now open the file and read it
     specData = prjPySpec.specDataFile(specFile)
@@ -83,23 +85,23 @@ def do_the_work(specFile, scanList, column_labels):
     for scanNum in scanList:
         outFile = makeOutputFileName(specFile, scanNum)
         # assume that this data file started with #S 1 (scan 1)
-        # TODO: since this might not be true for some files, make more robust
-        # better to implement this into prjPySpec as noted there
         scan = specData.scans[scanNum-1]
+        # TODO: since this might not be true for some files, make this more robust.
+        # Better to make an interface in prjPySpec and call that here:
+        #   scan = specData.get_scan(scanNum)
     
         # get the column numbers corresponding to the column_labels
 	column_numbers = []
 	for label in column_labels:
 	    if label in scan.L:
+	        # report all columns in order specified on command-line
 	        column_numbers.append( scan.L.index(label) )
 	    else:
 	        msg = 'column label "' + label + '" not found in scan #'
 	        msg += str(scanNum) + ' ... skipping'
-	        print msg
-	        column_numbers = None
-	        break
+	        print msg       # report all mismatched column labels
     
-        if column_numbers is not None:
+        if len(column_numbers) == len(column_labels):   # must be perfect matches
             txt = ['# ' + '\t'.join(column_labels), ]
             for data_row in scan.data_lines:
                 data_row = data_row.split()
@@ -114,7 +116,17 @@ def do_the_work(specFile, scanList, column_labels):
 
 
 def parseCmdLine(cmdArgs):
-    '''interpret the command-line arguments (avoid getopts, optparse, and argparse)'''
+    '''
+    interpret the command-line arguments (avoid getopts, optparse, and argparse)
+    
+    :param [str] cmdArgs: command line arguments split into a list, as in sys.argv
+    
+    :returns tuple: specFile, scanList, column_labels
+    
+    :param str specFile: name of existing SPEC data file to be read
+    :param [int] scanList: list of chosen SPEC scan numbers
+    :param [str] column_labels: list of column labels
+    '''
 
     if len(cmdArgs) < 4:
         print "usage: extractSpecScan.py specFile scanNum [scanNum [...]] col1 [col2 [col3 [...]]]"
@@ -133,20 +145,26 @@ def parseCmdLine(cmdArgs):
     scanList = []
     while True:
         try:
-            scanList.append( int(cmdArgs[pos]) )
+            scanNum = int(cmdArgs[pos])
+            if scanNum not in scanList:
+                # avoid reporting the same scan number more than once
+                scanList.append( scanNum )
             pos += 1
         except ValueError:
             break
 
     # variable length argument
     # all that is left on the line are column labels
-    column_labels = cmdArgs[pos:]
+    column_labels = []
+    for label in cmdArgs[pos:]:
+        if label not in column_labels:
+            # avoid reporting the same column label more than once
+            column_labels.append( label )
     return specFile, scanList, column_labels
 
 
 def main():
-    specFile, scanList, column_labels = parseCmdLine(sys.argv)
-    do_the_work(specFile, scanList, column_labels)
+    extractScans(sys.argv)
 
 
 def test():
@@ -154,13 +172,12 @@ def test():
     #  /home/beams/IXS/Data/HERIX/SPEC/2013-1/Weber/CeCoIn5
     # copy it and use the scratch copy (to be safe)
     # cp /home/beams/IXS/Data/HERIX/SPEC/2013-1/Weber/CeCoIn5 /tmp
-    cmdLine = sys.argv[0] + r' /tmp/CeCoIn5   5   HerixE Ana5 ICO-C'
+    cmdLine = sys.argv[0] + r' /tmp/CeCoIn5   5 6 7 8 9   HerixE Ana5 ICO-C'
 
     # alternate test data from USAXS archive
-    cmdLine = sys.argv[0] + r' ./testdata/11_03_Vinod.dat   2 12   USAXS.m2rp Monitor  I0'
+    cmdLine = sys.argv[0] + r' ./testdata/11_03_Vinod.dat   1 2 12   USAXS.m2rp Monitor  I0'
     
-    specFile, scanList, column_labels = parseCmdLine(cmdLine.split())
-    do_the_work(specFile, scanList, column_labels)
+    extractScans(cmdLine.split())
 
 
 if __name__ == "__main__":
