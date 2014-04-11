88082c7637147ce10448180d2cbe3f975f23b218
Pete R Jemian <prjemian@users.noreply.github.com>
2014-04-11 20:56:22 +0000

    BCDA_15ID-1

--- try_reduce.py
+++ try_reduce.py
@@ -10,10 +10,12 @@ from specplot import *
 import spec2nexus
 
 def main():
-    os.chdir('testdata/2014-04-09')
+    os.chdir('testdata/2014-04')
     sd = spec2nexus.prjPySpec.SpecDataFile('04_09_Prakash.dat')
     os.chdir('04_09_Prakash_fly')
-    for hdf_file_name in glob.glob('*.h5'):
+    print 'SPEC data file: ', sd.fileName
+    for hdf_file_name in sorted(glob.glob('*.h5')):
+        print '   FlyScan data file: ', hdf_file_name
         scanNum = int(hdf_file_name[1:hdf_file_name.find('_')])
         scan = sd.getScan(scanNum)
         hdf = reduceFlyData.UsaxsFlyScan(hdf_file_name)
@@ -22,9 +24,10 @@ def main():
         pl = format_ploticus_data(plotData)
         dataFile = write_ploticus_data_file(pl['data'])
         plotFile = 'S%d_plot.png' % scanNum
-     
+        
         #---- execute the ploticus command file using a "prefab" plot style
         run_ploticus_command_script(scan, dataFile, plotData, plotFile)
+        print '   FlyScan plot file: ', plotFile
 
 
 if __name__ == '__main__':
