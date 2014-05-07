5dab7f765d093259b84567447dc68a14f425cee1
Pete R Jemian <prjemian@users.noreply.github.com>
2014-05-07 04:45:47 +0000

    compute mean and std deviation, prepare for rebinning, smoother API, BCDA_15ID-13

--- testdata/flyScanHdf5Files/round3.py
+++ testdata/flyScanHdf5Files/round3.py
@@ -23,6 +23,18 @@ class UsaxsFlyScan(object):
     def __init__(self, hdf5_file_name):
         if not os.path.exists(hdf5_file_name):
             raise IOError, 'file not found: ' + hdf5_file_name
+
+        self.units = dict(
+            ar = 'degrees',
+            upd_ranges = '',
+            qVec = '1/A',
+            rVec = 'none',
+            drVec = 'none', 
+            rVec_max = 'none',
+            centroid = 'degrees',
+            fwhm = 'degrees',
+        )
+
         self.hdf5_file_name = hdf5_file_name
         self.reduced = self.read_reduced()
         if 'full' not in self.reduced:
@@ -34,7 +46,6 @@ class UsaxsFlyScan(object):
             raise IOError, 'file not found: ' + self.hdf5_file_name
         hdf = h5py.File(self.hdf5_file_name, 'r')
     
-        d2r = math.pi / 180
         wavelength = float(hdf['/entry/instrument/monochromator/wavelength'][0])
         ar_center  = float(hdf['/entry/metadata/AR_center'][0])
     
@@ -48,11 +59,11 @@ class UsaxsFlyScan(object):
         AR_start =          float(raw['AR_start'][0])
         AR_increment =      float(raw['AR_increment'][0])
         raw_ar = AR_start - numpy.arange(raw_num_points) * AR_increment
+        d2r = math.pi / 180
         qVec = (4*math.pi/wavelength) * numpy.sin(d2r*(ar_center - raw_ar)/2.0)
         
         amp_name = self.get_USAXS_PD_amplifier_name(hdf)
         upd_ranges = self.get_ranges(hdf, amp_name)
-        V_f_gain = FIXED_VF_GAIN
         gains = self.get_gain(hdf, amp_name)
         bkg   = self.get_bkg(hdf, amp_name)
         
@@ -60,48 +71,76 @@ class UsaxsFlyScan(object):
         upd_gain = numpy.ma.masked_less_equal(upd_gain, 0)
         upd_dark = numpy.array([0,] + bkg)[upd_ranges.data+1]
         upd_dark = numpy.ma.masked_less_equal(upd_dark, 0)
+        V_f_gain = FIXED_VF_GAIN
         pulse_frequency = raw['mca_clock_frequency'][0] or  MCA_CLOCK_FREQUENCY
         channel_time_s = raw_clock_pulses / pulse_frequency
         rVec = (raw_upd - channel_time_s*upd_dark) / upd_gain / raw_I0 / V_f_gain
-        centroid = numpy.sum(rVec*raw_ar) / numpy.sum(rVec)
         
-        fwhm = 0.0        # TODO: compute FWHM
+        centroid, sigma = self.mean_sigma(raw_ar, rVec)
+        fwhm = sigma * 2 * math.sqrt(2*math.log(2.0))
     
         hdf.close()
         
         full = dict(
-            ar = raw_ar,
-            upd_ranges = upd_ranges,
-            qVec = qVec,
-            rVec = rVec,
+            ar = self.remove_masked_data(raw_ar, rVec.mask),
+            #upd_ranges = self.remove_masked_data(upd_ranges, rVec.mask),
+            qVec = self.remove_masked_data(qVec, rVec.mask),
+            rVec = self.remove_masked_data(rVec, rVec.mask),
+            rVec_max = rVec.max(),
             centroid = centroid,
             fwhm = fwhm,
         )
         self.reduced = dict(full = full)
+
+#     def rebin(self, bin_count = None):
+#         '''generate R(Q) with a bin_count bins, save in ``self.reduced[str(bin_count)]`` dict'''
+#         bin_count = bin_count or self.bin_count
+#         s = str(bin_count)
+#         if s in self.reduced:
+#             return self.reduced[s]
+#         # TODO: rebin work here
+#         reduced = dict(
+#             qVec = qVec,
+#             rVec = rVec,
+#             drVec = drVec,
+#         )
+#         self.reduced[s] = reduced
+#         return reduced
 
     def read_reduced(self):
         '''
         read any and all reduced data from the HDF5 file, return in a dictionary
         
         dictionary = {
-          'full': dict(Q, R, R_max, AR, AR_FWHM, AR_centroid)
-          '250':  dict(Q, R, dR)
-          '5000': dict(Q, R, dR)
+          'full': dict(qVec, rVec, rVec_max, ar, fwhm, centroid)
+          '250':  dict(qVec, rVec, drVec)
+          '5000': dict(qVec, rVec, drVec)
         }
         '''
-        fields = ('Q', 'R', 'dR', 'R_max', 'AR', 'AR_centroid', 'AR_FWHM')
+        fields = self.units.keys()
         reduced = {}
         hdf = h5py.File(self.hdf5_file_name, 'r')
         entry = hdf['/entry']
         for key in entry.keys():
             if key.startswith('flyScan_reduced_'):
                 nxdata = entry[key]
-                reduction_name = key[len('flyScan_reduced_'):]
-                # extract the Numpy structure!
+                nxname = key[len('flyScan_reduced_'):]
                 d = {dsname: nxdata[dsname] for dsname in fields if dsname in nxdata}
-                reduced[reduction_name] = d
+                reduced[nxname] = d
         hdf.close()
         return reduced
+    
+    def save(self, hfile, key):
+        if key not in self.reduced:
+            return
+        ds = self.reduced[key]
+        nxname = 'flyScan_reduced_' + key
+        hdf = h5py.File(hfile, 'a')
+        nxentry = eznx.openGroup(hdf, 'entry', 'NXentry')
+        nxdata = eznx.openGroup(nxentry, nxname, 'NXdata')
+        for key in sorted(ds.keys()):
+            eznx.write_dataset(nxdata, key, ds[key], units=self.units[key])
+        hdf.close()
 
     def get_USAXS_PD_amplifier_name(self, hdf):
         base = '/entry/flyScan/upd_flyScan_amplifier'
@@ -160,7 +199,6 @@ class UsaxsFlyScan(object):
         :param obj hdf: opened HDF5 file instance
         :param str amplifier: amplifier name, as stored in the HDF5 file
         '''
-        # TODO: consider the numpy array.where() method with a ranges array with just the changes
         def load_gain(x):
             return hdf[base+str(x)][0]
         base = '/entry/metadata/' + amplifier + '_gain'
@@ -181,22 +219,38 @@ class UsaxsFlyScan(object):
         base = '/entry/metadata/' + amplifier + '_bkg'
         bkg = map(load_bkg, range(5))
         return bkg
+    
+    def mean_sigma(self, x, w):
+        '''
+        return mean and standard deviation of weighted x array
+        
+        :param numpy.array x: array to be averaged
+        :param numpy.array w: array of weights
+        '''
+        xw = x * w
+        sumX = numpy.sum(xw)
+        sumXX = numpy.sum(xw * xw)
+        sumWt = numpy.sum(w)
+        centroid = sumX / sumWt
+        try:
+            sigma = math.sqrt( (sumXX - (sumX*sumX)/sumWt) / (sumWt - 1) )
+        except:
+            sigma = 0.0
+        return centroid, sigma
+    
+    def remove_masked_data(self, data, mask):
+        '''remove all masked data'''
+        arr = numpy.ma.masked_array(data=data, mask=mask)
+        return arr.compressed()
 
 
 def main(hfile):
     ufs = UsaxsFlyScan(hfile)
-    full = ufs.reduced['full']
-
-    hdf = h5py.File('reduced.h5', 'w')
-    nxentry = eznx.openGroup(hdf, 'entry', 'NXentry')
-    nxdata = eznx.openGroup(nxentry, 'reduced', 'NXdata')
-    eznx.write_dataset(nxdata, 'ar',            full['ar'],         units='degrees')
-    eznx.write_dataset(nxdata, 'upd_ranges',    full['upd_ranges'], units='masked')
-    eznx.write_dataset(nxdata, 'qVec',          full['qVec'],       units='1/A')
-    eznx.write_dataset(nxdata, 'rVec',          full['rVec'],       units='none')
-    eznx.write_dataset(nxdata, 'centroid',      full['centroid'],   units='degrees')
-    hdf.close()
+    ufs.reduce()
+    ufs.save('reduced.h5', 'full')
 
 
 if __name__ == '__main__':
     main('S563_PB_GRI_9_Nat_200C.h5')
+    #main('S555_PB_GRI_9_Nat_175C.h5')
+    #main('S571_Heater_Blank.h5')
