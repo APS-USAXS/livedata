ab11f724fd09d7a6f8334c81da219e25ab2a1d39
Pete R Jemian <prjemian@users.noreply.github.com>
2014-05-08 04:47:20 +0000

    modified USAXS step scan algorithm completed, BCDA_15ID-13

--- testdata/flyScanHdf5Files/ustep.py
+++ testdata/flyScanHdf5Files/ustep.py
@@ -7,40 +7,91 @@ Step-Size Algorithm for Bonse-Hart Ultra-Small-Angle Scattering Instruments
 '''
 
 
-import math
-
-
-def ustep(start, center, finish, numPts, factor, exponent, minStep):
+class ustep(object):
     '''
-    return a list of positions between start and finish
-    
+    find the series of positions for the USAXS
+
     :param float start: required first position of list
     :param float center: position to take minStep
     :param float finish: required ending position of list
     :param float numPts: length of the list
-    :param float factor: :math:`k`, multiplying factor
     :param float exponent: :math:`\eta`, exponential factor
     :param float minStep: smallest allowed step size
+    :param float factor: :math:`k`, multiplying factor (computed internally)
+    :param [float] series: computed list of positions
     '''
-# def uascanTestSeries(start, center, finish, numPts, factor, exponent, minStep) '{
-#   local x step i dir
-# 
-#   x = start
-#   dir = (start < finish) ? 1 : -1
-#   for (i=1; i < numPts; i++) {
-#     step = uascanStepFunc(x, factor, center, exponent, minStep)
-#     x += dir*step
-#     if ( ((dir>0) && (x > finish))  ||  ((dir<0) && (x < finish)) ) {
-#       return i
-#     }
-#   }
-#   return (-1 * fabs(x - finish))
-# }'
-    pass
+    
+    def __init__(self, start, center, finish, numPts, exponent, minStep):
+        self.start = start
+        self.center = center
+        self.finish = finish
+        self.numPts = numPts
+        self.exponent = exponent
+        self.minStep = minStep
+        self.sign = {True: 1, False: -1}[start < finish]
+        self.series = []
+        self.factor = self.find_factor()
+        
+    def find_factor(self):
+        '''
+        Determine the factor that will make a series with the specified parameters.
+        
+        Choose the factor that will minimize :math:`| x_n - finish |` subject to:
+        
+        .. math::
+        
+           x_1 = start
+           x_n <= finish
+        
+        This routine CAN FAIL if :math:`(finish - start)/minStep >= numPts`
+        '''
+        span_target = abs(self.finish - self.start)
+        span_precision = abs(self.minStep) * 0.2
+        factor = abs(self.finish-self.start) / (self.numPts -1)
+        fStep = factor
+        larger = 3.0
+        smaller = 0.5
+        for _ in range(100):
+            self.make_series(factor)
+            span = abs(self.series[0] - self.series[-1])
+            span_diff = span - span_target
+            if abs(span_diff) <= span_precision:
+                break
+            if span_diff < 0:
+                fStep = abs(fStep) * larger
+            else:
+                fStep = -abs(fStep) * smaller
+            factor += fStep
+        return factor
+    
+    def make_series(self, factor):
+        '''create self.series with the given factor'''
+        x = self.start
+        series = [x, ]
+        for _ in range(self.numPts - 1):
+            x += self.sign * self.uascanStepFunc(x, factor)
+            series.append(x)
+        self.series = series
+    
+    def uascanStepFunc(self, x, factor):
+        '''Calculate the next step size with the given parameters'''
+        if abs(x - self.center) > 1e100:
+            step = 1e100
+        else:
+            step = factor * pow( abs(x - self.center), self.exponent ) + self.minStep
+        return step
 
 
 def main():
-    pass
+    start = 10.0
+    center = 9.5
+    finish = 7
+    numPts = 100
+    exponent = 1.2
+    minStep = 0.0001
+    u = ustep(start, center, finish, numPts, exponent, minStep)
+    print u.factor
+    print u.series
 
 
 if __name__ == '__main__':
@@ -54,180 +105,3 @@ if __name__ == '__main__':
 # $URL$
 # $Id$
 ########### SVN repository information ###################
-
-# SPEC implementation
-'''
-#
-#  usaxs_uascan.mac
-#
-########### SVN repository information ###################
-# :HeadURL: https://subversion.xray.aps.anl.gov/spec/beamline_config/trunk/usaxs/usaxs/usaxs_uascan.mac
-########### SVN repository information ###################
-
-# these are now epics variables
-#global     AR_VAL_CENTER                 # center value for AR stage
-#global     NUMPTS                        # number of points
-#global     UATERM                        # exponent... Changes spread of the log stepping
-#global     START_OFFSET                  # Q offset to start above AR_VAL_CENTER
-#global     FINISH                        # end angle for USAXS scans
-#global     USAXS_MINSTEP                 # min step of uascan
-#
-
-###################################################################################
-####
-####  general scan macro for USAXS for both 1D & 2D collimations
-####
-def USAXSscan '{
-  # decide if we are scaning up or down...
-  if(USAXSScanUp) {
-        # scanning up, new method
-        Finish_in_Angle = AR_VAL_CENTER + (360/PI)*asin( FINISH * _USAXS_Lambda / (4*PI))
-        # START = AR_VAL_CENTER - START_OFFSET
-        # 2013-2-9 Changed to START_OFFSET in Q and need negative value to start above/below the peak now.
-        START = AR_VAL_CENTER + (360/PI)*asin( START_OFFSET * _USAXS_Lambda / (4*PI))
-  } else {
-        # scanning down, old method
-        Finish_in_Angle = AR_VAL_CENTER - (360/PI)*asin( FINISH * _USAXS_Lambda / (4*PI))
-        #START = AR_VAL_CENTER + START_OFFSET
-        # 2013-2-9 Changed to START_OFFSET in Q and need negative value to start above/below the peak now.
-        START = AR_VAL_CENTER - (360/PI)*asin( START_OFFSET * _USAXS_Lambda / (4*PI))
-  }
-
-  uascan ar START AR_VAL_CENTER Finish_in_Angle MINSTEP DY0 SDD AY0 SAD UATERM NUMPNTS TIME
-
-'
-
-#########################################################################
-#
-# `uascan' is a single-motor scan with adjustable step size
-#
-def uascan '
-  if ($# != 12) {
-    printf("Usage: uascan %s %s %s\n",         \
-       "motor start center finish minStep",    \
-       "dy0 SDD_mm ay0 SAD_mm",                \
-       "exponent intervals time")
-    exit
-  }
-
-
-    # find appropriate factor for weighing the points
-    k = uascanFindFactor(start, center, finish, intervals, exponent, minStep)
-    # ...
-    _pos = start
-    
-    # ...
-    _pos += _dir * uascanStepFunc(_pos, k, center, exponent, minStep)
-    # ...
-'
-
-#############################################################################
-#
-#    subprograms....
-#
-# uascanStepFunc
-#
-# Calculate the next step size with the given parameters
-#
-uascanStepDebug = 0
-def uascanStepFunc(x, factor, center, exponent, minStep) '{
-  local step
-  step = factor * pow( fabs(x - center), exponent ) + minStep
-  if (uascanStepDebug >= 2)
-    printf ("step = %g\n", step)
-  return step
-}'
-
-# uascanTestSeries
-#
-# Make a series with the specified parameters
-#  If factor is too large, return the number of points required to reach finish.
-#  Otherwise, return the closeness of the final point to the finish.
-#
-def uascanTestSeries(start, center, finish, numPts, factor, exponent, minStep) '{
-  local x step i dir
-
-  x = start
-  dir = (start < finish) ? 1 : -1
-  for (i=1; i < numPts; i++) {
-    step = uascanStepFunc(x, factor, center, exponent, minStep)
-    x += dir*step
-    if ( ((dir>0) && (x > finish))  ||  ((dir<0) && (x < finish)) ) {
-      return i
-    }
-  }
-  return (-1 * fabs(x - finish))
-}'
-
-# uascanFindFactor
-#
-# Determine the factor that will make a series with the specified parameters.
-#  Choose the factor that will minimize | x[n] - finish |  subject to:
-#    x[1] = start
-#    x[n] <= finish
-# This routine CAN FAIL if (finish - start)/minStep >= numPts
-#
-def uascanFindFactor(start center finish numPts exponent minStep) '{
-  local factor fStep result f2 r2 i
-
-  factor = 0
-  result = uascanTestSeries(start, finish, numPts, factor, center, exponent, minStep)
-  if (result > 0) {
-    printf ("With factor=%g, could not get correct result (%g)\n", factor, result)
-    #
-    # fail gracefully with factor
-    # let calling routine decide what to do
-    # (usually just step with factor = 0 until x[i] > finish)
-    #
-    return (factor)
-  }
-  #
-  # Initially, choose factor = abs(finish - start) / (numPts - 1)
-  # call uascanTestSeries with factor
-  #  if result<0, then factor *= 10 and repeat
-  # then continue as below
-  #
-#  r2 = -1
-#  fStep = abs(finish-start) / (numPts -1)
-#  while (r2 < 0) {
-#    f2 = factor + fStep
-#    r2 = uascanTestSeries(start, center, finish, numPts, f2, exponent, minStep)
-#    if (uascanStepDebug >= 1)
-#      printf("factor = %g  result = %g\n", f2, r2)
-#    if (r2 < 0) fStep *= 10
-#  }
-  fStep = 10
-  #
-  # 40 times means better than 12-digit precision
-  #
-  for (i = 1; i < 40; i++) {
-    f2 = factor + fStep
-    r2 = uascanTestSeries(start, center, finish, numPts, f2, exponent, minStep)
-    if ( (r2 <= 0) && ( fabs(r2) < fabs(result) ) ) {
-      factor = f2
-      result = r2
-    } else {
-      fStep *= 0.5
-    }
-    if (uascanStepDebug >= 1)
-      printf ("%d: f=%0.15g  result=%0.15g\n", i, factor, result)
-    if ( fabs(result) < 0.5*fabs(minStep) ) break
-  }
-  return (factor)
-}'
-
-
-#global     _n1     # number of intervals
-#global     _nm     # number of motors
-#global     _m[]    # motor number
-#global     _s[]    # start position
-#global     _f[]    # finish position
-#global     _d[]    # scale factor
-#global     _c[]    # center position
-#global     _ms[]   # minimum step size
-#global     _dy0    # photodiode Y position at center angle of scan
-#global     _ay0    # analyzer stage Y position at center angle of scan
-#global     _exp    # exponent for step function
-#global     _dir[]  # direction for motor to move
-#global     _pos[]  # present function (while step scanning)
-'''
