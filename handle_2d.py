be1a9599a646dab83ab8f71cb137e7d28b660623
Pete R Jemian <prjemian@users.noreply.github.com>
2014-06-14 03:30:30 +0000

    implementation needs to be done on usaxscontrol2, the basics are laid out but not enabled.

--- handle_2d.py
+++ handle_2d.py
@@ -12,16 +12,16 @@ import matplotlib.pyplot as plt
 
 
 PATH_TO_HERE = os.path.abspath(os.path.split(__file__)[0])
-TEST_FILE = os.path.join(PATH_TO_HERE, 'waxs', 'LSM_YSZ_infl_227.hdf5')
+TEST_FILE = os.path.join(PATH_TO_HERE, 'testdata', '2014-06-12', 'waxs', 'LSM_YSZ_infl_227.hdf5')
 HDF5_DATA_PATH = '/entry/data/data'
 SCALING_FACTOR = 1        #  2**24
 PLOT_H_INT = 7
 PLOT_V_INT = 3
 COLORMAP = 'cubehelix'        # http://matplotlib.org/api/pyplot_summary.html#matplotlib.pyplot.colormaps
-PNG_FILE = 'image.png'
+IMG_FILE = 'image.png'
 
 
-def make_png(h5file, pngfile, h5path=HDF5_DATA_PATH, 
+def make_png(h5file, imgfile, h5path=HDF5_DATA_PATH, log_image=True,
              hsize=PLOT_H_INT, vsize=PLOT_V_INT, cmap=COLORMAP):
     '''
     read the image from the named HDF5 file and make a PNG file
@@ -33,13 +33,14 @@ def make_png(h5file, pngfile, h5path=HDF5_DATA_PATH,
     The HDF5 file could be a NeXus file, not required though.
     
     :param str h5file: name of HDF5 file (path is optional)
-    :param str pngfile: name of PNG file to be written (path is optional)
+    :param str imgfile: name of image file to be written (path is optional)
     :param str h5path: path to the image dataset within the HDF5 file
        (default = '/entry/data/data', common for some NeXus data files)
+    :param bool log_image: plot log(image)
     :param int hsize: horizontal size of the PNG image (default: 7)
     :param int hsize: vertical size of the PNG image (default: 3)
     :param str cmap: colormap for the image (default: 'cubehelix'), 'jet' is another good one
-    :return str: *pngfile*
+    :return str: *imgfile*
     :raises: IOError if either *h5file* or *h5path* are not found
     '''
     if not os.path.exists(h5file):
@@ -56,20 +57,30 @@ def make_png(h5file, pngfile, h5path=HDF5_DATA_PATH,
         raise IOError, msg
 
     image_data = numpy.ma.masked_less_equal(ds.value, 0)
-    image_data = image_data.filled(image_data.min())    # replace masked data with min good value
-    #meta = dict(hi=image_data.max(), lo=image_data.min(), h=image_data.shape[0], w=image_data.shape[1])
-    log_image = numpy.log(image_data)
-    log_image -= log_image.min()
-    log_image *= SCALING_FACTOR / log_image.max()
+    # replace masked data with min good value
+    image_data = image_data.filled(image_data.min())
+    if log_image:
+        image_data = numpy.log(image_data)
+        image_data -= image_data.min()
+        image_data *= SCALING_FACTOR / image_data.max()
 
     plt.set_cmap(cmap)
     fig = plt.figure(figsize=(hsize, vsize))
     ax = fig.add_subplot(111)
     ax.cla()
-    ax.imshow(log_image, interpolation='nearest')
-    fig.savefig(pngfile)
-    return pngfile
+    ax.imshow(image_data, interpolation='nearest')
+    fig.savefig(imgfile)
+    return imgfile
 
 
 if __name__ == '__main__':
-    make_png(TEST_FILE, PNG_FILE)
+    make_png(TEST_FILE, IMG_FILE)
+
+
+########### SVN repository information ###################
+# $Date$
+# $Author$
+# $Revision$
+# $URL$
+# $Id$
+########### SVN repository information ###################
