e51b131197d33b69f4385f9127f13b9220410a80
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2014-08-06 22:02:25 +0000

    test data for BCDA_15ID-67

--- USAXS/livedata/developer_test_reduceFlyData.py
+++ USAXS/livedata/developer_test_reduceFlyData.py
@@ -11,6 +11,6 @@ sys.argv = [sys.argv[0],]
 sys.argv.append('')
 
 import glob
-for hdf5_file in glob.glob('testdata/*.h5'):
+for hdf5_file in glob.glob('testdata/2014-07/08_04_NegDirCorr_fly/*.h5'):
   sys.argv[1] = hdf5_file
-  reduceFlyData.main()
+  reduceFlyData.command_line_interface()
