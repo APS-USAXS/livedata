63efa60eac3d31574d42d9065e772413ce579263
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2014-08-13 21:03:41 +0000

    BCDA_15ID-67: handles fly scan mode = 1 (Array, most common case now)

--- USAXS/livedata/developer_test_reduceFlyData.py
+++ USAXS/livedata/developer_test_reduceFlyData.py
@@ -11,8 +11,12 @@ sys.argv = [sys.argv[0],]
 sys.argv.append('')
 
 testglob = 'testdata/flyscan_modes/*.h5'
-testglob = 'testdata/flyscan_modes/*S18_FS_Fixed.h5'
-testglob = 'testdata/*S563_PB_GRI_9_Nat_200C.h5'
+testglob = 'testdata/flyscan_modes/S18_FS_Fixed*.h5'    # from day with no beam, this dataset has problems!
+testglob = 'testdata/flyscan_modes/*_PSO_Fixed*.h5'     # from day with no beam 
+testglob = 'testdata/flyscan_modes/S39_Blank*.h5'       # 2014-08-13, fly scan mode=1
+testglob = 'testdata/flyscan_modes/S44_GC_Adam*.h5'     # 2014-08-13, fly scan mode=1
+# testglob = 'testdata/flyscan_modes/S19_FS_Fixed*.h5'
+# testglob = 'testdata/S563_PB_GRI_9_Nat_200C*.h5'
 
 import glob
 for hdf5_file in glob.glob(testglob):
