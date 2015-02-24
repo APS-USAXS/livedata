c13e415d697744374aa5a2496e5ce6fd9813864a
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2015-02-24 02:14:40 +0000

    revised computation for Q=0

--- USAXS/livedata/reduceFlyData.py
+++ USAXS/livedata/reduceFlyData.py
@@ -234,9 +234,7 @@ class UsaxsFlyScan(object):
         elif mode_number in (AR_MODE_ARRAY, AR_MODE_TRAJECTORY):
             keymap = {AR_MODE_ARRAY: '/entry/flyScan/AR_PulsePositions',    # a few thousand points
                       AR_MODE_TRAJECTORY: '/entry/flyScan/AR_waypoints'}    # a few hundred points
-            raw_ar = hdf[keymap[mode_number]]
-            ar_adjustment = ar_center - raw_ar[0]
-            raw_ar += ar_adjustment
+            raw_ar = ar_center - (hdf[keymap[mode_number]] - raw_ar[0] + AR_start)
             if len(raw_ar) > raw_num_points:
                 raw_ar = raw_ar[:raw_num_points]   # truncate unused bins, if needed
             
