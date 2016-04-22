9f9a2ea566f22d346d9a0490159228f449b16ede
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2016-04-22 20:12:40 +0000

    adjustments

--- USAXS/livedata/plot_mpl.py
+++ USAXS/livedata/plot_mpl.py
@@ -13,7 +13,7 @@ BISQUE_RGB    = (255./255, 228./255, 196./255)  # 255 228 196 bisque
 MINTCREAM_RGB = (245./255, 255./255, 250./255)  # 245 255 250 MintCream
 
 SYMBOL_LIST = ("^", "D", "s", "v", "d", "<", ">")
-COLOR_LIST = "orange lime green blue purple violet chocolate gray black".split() # red is NOT in the list
+COLOR_LIST = "orange yellow lime green blue purple violet chocolate gray black".split() # red is NOT in the list
 
 CHART_FILE = 'livedata.png'
 
