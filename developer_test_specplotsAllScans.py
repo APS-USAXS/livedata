39a1b77d16a4f49481ed79591b939be3bbc4d82d
Pete Jemian <prjemian@gmail.com>
2019-07-10 21:55:55 -0500

    TST #28

--- developer_test_specplotsAllScans.py
+++ developer_test_specplotsAllScans.py
@@ -1,20 +1,30 @@
 
 '''developer routine to test specplotsAllScans.py'''
 
+import logging
 import os
+import shutil
 import specplotsAllScans
 import sys
-import logging
 
 logger = logging.getLogger("pydev-shell")
 logger.setLevel(logging.DEBUG)
+
+#  SPEC2NEXUS_PLUGIN_PATH
+#  os.environ.get(env_var_name, None)
 
 # note: made soft link to simulate this path
 # mkdir -p /home/mintadmin/Documents/usaxs_sim/USAXS_data
 # cd /
 # ln -s /home/mintadmin/Documents/usaxs_sim ./share1
-path = "/share1/USAXS_data"
+path = "/share1/USAXS_data/2019-06"
 
 sys.argv.append(os.path.join(path, "06_26_NXSchool0626.dat"))
 
+f = "/share1/local_livedata/specplots/mtime_cache.txt"
+if os.path.exists(f):
+    os.remove(f)
+f = "/share1/local_livedata/specplots/2019/06/06_26_NXSchool0626"
+if os.path.exists(f):
+    shutil.rmtree(f, ignore_errors=True)
 specplotsAllScans.main()
