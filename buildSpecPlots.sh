a1efdcb73f66a6077b0765cf3f109edf42b13253
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2020-08-05 14:19:38 -0500

    DOC mark TODO items

--- buildSpecPlots.sh
+++ buildSpecPlots.sh
@@ -22,7 +22,9 @@ LOGFILE=/share1/local_livedata/specplots/specplots.log
 PYTHON=/APSshare/anaconda/x86_64/bin/python
 HDF5_DISABLE_VERSION_CHECK=2
 
-#
+# TODO: remove SPEC_DATA_PATTERN and all references
+#  Code below uses recent_spec_data_files.py
+
 # change the SPEC_DATA_PATTERN periodically to reduce the search time
 # this is very important, for example, just this one directory:
 #   /data/USAXS_data/2013-1*/*.dat  takes about a minute to run
@@ -38,6 +40,7 @@ HDF5_DISABLE_VERSION_CHECK=2
 SPEC_DATA_PATTERN=/share1/USAXS_data/2020-08/*.dat
 #SPEC_DATA_PATTERN=/share1/USAXS_data/2019-*/*.dat
 
+# TODO: remove ploticus and all references
 #export PLOTICUS_BASE=/home/beams/USAXS/Documents/ploticus/pl241src
 #export PLOTICUS_PREFABS=$PLOTICUS_BASE/prefabs
 
@@ -45,12 +48,14 @@ cd $CODE_DIR
 echo "#= $$ --Start--- `/bin/date`" >> $LOGFILE 2>&1
 
 #filelist=`/bin/ls -1 $SPEC_DATA_PATTERN`
-# 2019-07-09, prj: look at scan logs and build the list
-filelist=`$PYTHON ./recent_spec_data_files.py`
 # for item in $filelist;
 # 	do
 # 	# echo $item >> $LOGFILE 2>&1;
 # 	$PYTHON $PROGRAM $item >> $LOGFILE 2>&1;
 # done
+
+# 2019-07-09, prj: look at scan logs and build the list
+filelist=`$PYTHON ./recent_spec_data_files.py`
+
 $PYTHON $PROGRAM $filelist >> $LOGFILE 2>&1;
 echo "#= $$ --Done---------------------------------- `/bin/date`" >> $LOGFILE 2>&1
