bba6eba20e007adc884d373105f94e636f57453c
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2020-08-08 14:12:21 -0500

    MNT #43 revise the test code

--- developer_test_specplotsAllScans.py
+++ developer_test_specplotsAllScans.py
@@ -3,6 +3,7 @@
 
 import logging
 import os
+import recent_spec_data_files
 import shutil
 import specplotsAllScans
 import sys
@@ -10,19 +11,23 @@ import sys
 logger = logging.getLogger("pydev-shell")
 logger.setLevel(logging.DEBUG)
 
-# note: made soft link to simulate this path
-# mkdir -p /home/mintadmin/Documents/usaxs_sim/USAXS_data
-# cd /
-# ln -s /home/mintadmin/Documents/usaxs_sim ./share1
-path = "/share1/USAXS_data/2019-06"
+# # note: made soft link to simulate this path
+# # mkdir -p /home/mintadmin/Documents/usaxs_sim/USAXS_data
+# # cd /
+# # ln -s /home/mintadmin/Documents/usaxs_sim ./share1
+# path = "/share1/USAXS_data/2019-06"
 
-sys.argv.append(os.path.join(path, "06_26_NXSchool0626.dat"))
-sys.argv.append(os.path.join(path, "06_27_NX2.dat"))
+# sys.argv.append(os.path.join(path, "06_26_NXSchool0626.dat"))
+# sys.argv.append(os.path.join(path, "06_27_NX2.dat"))
 
-f = "/share1/local_livedata/specplots/mtime_cache.txt"
-if os.path.exists(f):
-    os.remove(f)
-f = "/share1/local_livedata/specplots/2019/"
-if os.path.exists(f):
-    shutil.rmtree(f, ignore_errors=True)
+# f = "/share1/local_livedata/specplots/mtime_cache.txt"
+# if os.path.exists(f):
+#     os.remove(f)
+# f = "/share1/local_livedata/specplots/2019/"
+# if os.path.exists(f):
+#     shutil.rmtree(f, ignore_errors=True)
+
+filelist = recent_spec_data_files.list_recent_spec_data_files()
+sys.argv += filelist
+
 specplotsAllScans.main()
