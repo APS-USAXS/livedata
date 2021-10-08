8f32417b78681991ad306d006299ae4dcc7a4a54
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2021-10-08 00:54:38 -0500

    MNT #50 implement lock file

--- buildSpecPlots.sh
+++ buildSpecPlots.sh
@@ -18,16 +18,24 @@
 CODE_DIR=/home/beams/USAXS/Documents/eclipse/USAXS/livedata
 PROGRAM=$CODE_DIR/specplotsAllScans.py
 LOGFILE=/share1/local_livedata/specplots/specplots.log
+LOCKFILE=/share1/local_livedata/specplots/buildSpecPlots.lock
 # NOTE: code is still python 2.7
 #PYTHON=/APSshare/epd/rh6-x86_64/bin/python
 PYTHON=/APSshare/anaconda/x86_64/bin/python
 HDF5_DISABLE_VERSION_CHECK=2
 
-cd $CODE_DIR
-echo "#= $$ --Start--- `/bin/date`" >> $LOGFILE 2>&1
+if test -f "${LOCKFILE}"; then
+    # echo "#= Found ${LOCKFILE}.  Another instance is running.  Exiting now. `/bin/date`" >> $LOGFILE 2>&1
+    echo "Locked, Another instance is running."
+else
+    touch "${LOCKFILE}"
+    cd $CODE_DIR
+    echo "#= $$ --Start--- `/bin/date`" >> $LOGFILE 2>&1
 
-# 2019-07-09, prj: look at scan logs and build the list
-filelist=`$PYTHON ./recent_spec_data_files.py`
+    # 2019-07-09, prj: look at scan logs and build the list
+    filelist=`$PYTHON ./recent_spec_data_files.py`
 
-$PYTHON $PROGRAM $filelist >> $LOGFILE 2>&1;
-echo "#= $$ --Done---------------------------------- `/bin/date`" >> $LOGFILE 2>&1
+    $PYTHON $PROGRAM $filelist >> $LOGFILE 2>&1;
+    /bin/rm "${LOCKFILE}"
+    echo "#= $$ --Done---------------------------------- `/bin/date`" >> $LOGFILE 2>&1
+fi
